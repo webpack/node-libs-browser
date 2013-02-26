4ee3d9123138692bca38663b7901d9ac47cbd77d
Roman Shtylman <shtylman@gmail.com>
2013-02-26 12:03:51 -0500

    Loaders are not a part of this library or test suite.
    Loaders use this library, not the other way around.

--- test/webpack/index.js
+++
@@ -1,5 +0,0 @@
-var context = require.context("../suite", false, /^\.\/.*\.test\.js$/);
-Object.keys = require("../../util/objectKeys");
-context.keys().forEach(function(test) {
-	context(test);
-});
